e3e1f2312bcd3b731bd6b534fe794d0a298eebaa
Alexlee-7 <chouchouxx7@gmail.com>
2026-07-06 11:58:26 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAHoPsCutnKBDNRdFRovzlgoVliodggUi0",
+  authDomain: "to-do-list-b78b2.firebaseapp.com",
+  projectId: "to-do-list-b78b2",
+  storageBucket: "to-do-list-b78b2.firebasestorage.app",
+  messagingSenderId: "281318917924",
+  appId: "1:281318917924:web:842c069f23f2ce786a95b1"
+};
